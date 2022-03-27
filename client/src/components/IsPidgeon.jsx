5d104debe5821dd922cca0a3396709f3a1727a8d
DannyWong <danny.96.wong@gmail.com>
2022-03-27 11:36:48 -0700

    finished ispigeon loading and checking (with bugs)

--- client/src/components/IsPidgeon.jsx
+++ client/src/components/IsPidgeon.jsx
@@ -5,43 +5,75 @@ class IsPidgeon extends React.Component {
     super(props);
     this.state = {
       model: null,
-      imgUrl: null
+      imgUrl: '',
+      show: false,
+      loaded: false,
+      tested: false,
+      pigeonResult: null
     };
     this.imgRef = React.createRef();
     this.isPidgeonTest = this.isPidgeonTest.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleLoad = this.handleLoad.bind(this);
   }
 
   isPidgeonTest(image) {
     //This function tests an image to see if it is a pidgeon.
     this.props.model.classify(image.current)
       .then(predictions => {
-        console.log( predictions[0].className === 'partridge' ? 'This is a Pidgeon' : 'This is not a Pidgeon');
-        console.log(predictions[0].className === 'partridge' ? 'Pidgeon Confidence: ' + predictions[0].probability : '');
-        //Yes I know a partridge is not a pidgeon,
-        //but I keep feeding the model Pidgeons and they keep coming out as partridges???
+        this.setState({model: predictions, tested: true})
+        // console.log( predictions[0].className === 'partridge' ? 'This is a Pidgeon' : 'This is not a Pidgeon');
+        // console.log(predictions[0].className === 'partridge' ? 'Pidgeon Confidence: ' + predictions[0].probability : '');
+        // //Yes I know a partridge is not a pidgeon,
+        // //but I keep feeding the model Pidgeons and they keep coming out as partridges???
       })
       .catch(err => {
         console.log(err);
       })
   }
 
+  handleLoad() {
+    if (this.state.imgUrl !== null || this.state.imgUrl.length !== 0 || this.state.imgUrl !== undefined) {
+      this.setState({ loaded: true, show: true })
+    }
+  }
+
+  handleChange(e) {
+    this.setState({ imgUrl: e.target.value })
+  }
+
   render(){
     return (
       <div>
-          <h1>Is this a Pidgeon?</h1>
-          <div className="img-wrapper">
-            <img
-              src="https://i.imgur.com/ZVJCqZp.jpeg"
-              ref={this.imgRef}
-              alt="bird(?)"
-              crossOrigin='anonymous'
-              className="img"
-            />
-          </div>
-          <button onClick={() => this.isPidgeonTest(this.imgRef)}>
-            Test
-          </button>
+        <form>
+          <input type="text" placeholder="please enter a url" onChange={e => this.handleChange(e)}/>
+          <input type="button" onClick={this.handleLoad} value="load a pigeon" />
+        </form>
+        <h1>Is this a Pidgeon?</h1>
+        <div>
+          {this.state.show ?
+            <div>
+              <div className="img-wrapper">
+                <img
+                  src={this.state.loaded ? `${this.state.imgUrl}` : null}
+                  ref={this.imgRef}
+                  alt="bird(?)"
+                  crossOrigin='anonymous'
+                  className="img"
+                />
+              </div>
+              <button onClick={() => this.isPidgeonTest(this.imgRef)}>Test</button>
+              {this.state.tested ?
+                <div>{this.state.model[0].className === 'partridge' ?
+                <h4>This is a Pidgeon: Pidgeon Confidence: {this.state.model[0].probability}</h4>
+                : <h4>This is not pidgeon</h4>}</div>
+              : null}
+
+            </div>
+        : <h3>please load an image!</h3>}
       </div>
+
+      </div >
     );
   }
 
